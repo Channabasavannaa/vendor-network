da710a514e39638229c16d54e054665b405d788e
Channabasavannaa <channabasavannamb@gmail.com>
2025-11-17 13:16:02 +0530

    teraform files

---
+++ terraform/variables.tf
@@ -0,0 +1,18 @@
+variable "aws_region" {}
+
+variable "app_name" {
+default = "vender-network"
+description = "The name of the application"
+}
+
+variable "public_subnet" {
+  default = ["10.10.100.0/24", "10.10.101.0/24"]
+}
+
+variable "private_subnet" {
+  default = ["10.10.0.0/24", "10.10.1.0/24"]
+}
+
+variable "avaliability_zones" {
+  default = ["ap-south-1a", "ap-south-1b"]
+}
